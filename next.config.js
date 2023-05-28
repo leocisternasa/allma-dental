276c2651c2352b0bc981a8ef94f6da86cd93ca15
Leonardo Cisternas Albornoz <leonardocisternasalbornoz@Leonardos-MacBook-Pro.local>
2023-05-28 15:20:45 +0200

    fixing bug

--- next.config.js
+++ next.config.js
@@ -4,27 +4,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  // Otras configuraciones que tengas aquí
 };
 
-module.exports = (phase, { defaultConfig }) => {
-  return {
-    ...defaultConfig,
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      // Agrega aquí las configuraciones personalizadas de Webpack
-      config.module.rules.push({
-        test: /\.(woff2?|ttf|eot|svg|png|jpg|gif|mp4|webm)$/,
-        use: {
-          loader: "file-loader",
-          options: {
-            publicPath: "/_next",
-            name: "static/media/[name].[hash].[ext]",
-          },
-        },
-      });
-
-      return config;
-    },
-    ...nextConfig,
-  };
-};
+module.exports = nextConfig;
